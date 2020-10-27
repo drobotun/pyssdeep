78a467b6c7d40b10a4fb39d6169cef4020f2a971
Drobotun <drobotun@xakep.ru>
2020-10-27 20:00:23 +0300

    Fixed conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,10 +49,39 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
-# The master toctree document.
-master_doc = 'index'
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# The encoding of source files.
+source_encoding = 'utf-8-sig'
+
+# The master toctree document.
+master_doc = 'index'
+
+latex_elements = {
+# The paper size ('letterpaper' or 'a4paper').
+#'papersize': 'letterpaper',
+
+# The font size ('10pt', '11pt' or '12pt').
+#'pointsize': '10pt',
+
+# Additional stuff for the LaTeX preamble.
+'preamble': '\\usepackage[utf8]{inputenc}',
+#'babel': '\\usepackage[russian]{babel}',
+'cmappkg': '\\usepackage{cmap}',
+'fontenc': '\usepackage[T1,T2A]{fontenc}',
+'utf8extra':'\\DeclareUnicodeCharacter{00A0}{\\nobreakspace}',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+  ('index', 'Sphinx.tex', 'GOST cryptographic function',
+   'Drobotun Evgeny', 'manual'),
+]
